016521d8ec3edf4f0961594164ffda389e8df889
oderathegreat <oderathegreat@gmail.com>
2022-09-11 16:31:13 +0300

    list row | recyclerview and new recipe activity

---
+++ app/src/main/java/com/example/recipeme/RecipeLayout.kt
@@ -0,0 +1,11 @@
+package com.example.recipeme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_layout)
+    }
+}
