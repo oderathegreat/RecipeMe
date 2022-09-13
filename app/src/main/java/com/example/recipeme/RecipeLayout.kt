506cc0621035c0ed536cb46cd462c7a590eac736
oderathegreat <oderathegreat@gmail.com>
2022-09-13 07:21:12 +0300

    instantiate our adapter and layout manager

--- app/src/main/java/com/example/recipeme/RecipeLayout.kt
+++ app/src/main/java/com/example/recipeme/RecipeLayout.kt
@@ -3,10 +3,13 @@ package com.example.recipeme
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
+import com.example.recipeme.data.RecipeAdapter
 import com.example.recipeme.model.Recipe
 import org.json.JSONArray
 import org.json.JSONException
@@ -15,6 +18,11 @@ import kotlin.math.log
 class RecipeLayout : AppCompatActivity() {
 
     var volleyRequest: RequestQueue? = null
+    var recipeList:ArrayList<Recipe>? = null
+    var recipeAdapter:RecipeAdapter? =null
+    var layoutManager: RecyclerView.LayoutManager? =null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -22,6 +30,10 @@ class RecipeLayout : AppCompatActivity() {
 
         //instantiate our request
         volleyRequest = Volley.newRequestQueue(this)
+
+        //instantiate our recipe list
+        recipeList = ArrayList<Recipe>()
+
 
         val url = "https://api.punkapi.com/v2/beers"
         fetchRecipe(url)
@@ -46,12 +58,18 @@ class RecipeLayout : AppCompatActivity() {
                        var beerdesc = drinksObj.getString("description")
                        var beerthumb = drinksObj.getString("image_url")
                         //Log.d("TAGLINE===>", tagline.toString()) working fine
-
                         //Construct our recipe object
                         var recipe = Recipe()
                         recipe.title = beertitle
                         recipe.descri = beerdesc
                         recipe.thumbnail = beerthumb
+
+                        recipeList!!.add(recipe)
+
+                        //Instantiate our adapter
+                        recipeAdapter = RecipeAdapter(recipeList!!, this)
+                        layoutManager = LinearLayoutManager(this)
+
 
 
                     }
